089f1701002a050a5264ec2a84966dec358c8856
amanueltefera1769 <amanuelat07@gmail.com>
2022-01-04 18:58:33 +0300

    default

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+ // adds default header or from header.php
+ get_header();
+?>
+<div class="container mt-2">
+<?php 
+if(have_posts()){
+    while(have_posts()){
+        the_post();
+        get_template_part('contents/content');
+      
+     }}?>
+</div>
+
+
+<?php
+ // adds default footer or from footer.php
+ get_footer();
+ 
+ ?>
