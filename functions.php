5cdb191be19537020bdf5f9c74656f0e4102cd33
Kerod-Fresenbet-Gebremedhin2660 <kerodgebremedhin@gmail.com>
2022-01-28 12:16:57 +0300

    added more misc  functions

--- functions.php
+++ functions.php
@@ -76,4 +76,147 @@
   	return $html;
   }
   add_filter('get_custom_logo', 'benfiratkaya_custom_logo_output');
+
+  function benfiratkaya_add_search_form($items, $args) {
+    if ($args->theme_location == 'primary') {
+      $searchForm = get_search_form(false);
+      $items      .= $searchForm;
+    }
+    return $items;
+  }
+  add_filter('wp_nav_menu_items', 'benfiratkaya_add_search_form', 10, 2);
+
+  function benfiratkaya_move_comment_field_to_bottom($fields) {
+    $comment_field = $fields['comment'];
+    unset($fields['comment']);
+    $fields['comment'] = $comment_field;
+    return $fields;
+  }
+  add_filter('comment_form_fields', 'benfiratkaya_move_comment_field_to_bottom');
+
+  function benfiratkaya_script_enqueue() {
+    wp_enqueue_style('bootstrap.min.css', get_template_directory_uri().'/assets/libs/bootstrap/css/bootstrap.min.css', array(), '4.5.0', 'all');
+    wp_enqueue_style('fancybox.min.css', get_template_directory_uri().'/assets/libs/fancybox/css/fancybox.min.css', array(), '3.5.7', 'all');
+    wp_enqueue_style('owl.carousel.min.css', get_template_directory_uri().'/assets/libs/owl.carousel/css/owl.carousel.min.css', array(), '2.3.4', 'all');
+    wp_enqueue_style('main.min.css', get_template_directory_uri().'/assets/css/main.min.css', array(), '1.0', 'all');
+    wp_enqueue_style('responsive.min.css', get_template_directory_uri().'/assets/css/responsive.min.css', array(), '1.0', 'all');
+    wp_enqueue_style('style-editor.css', get_template_directory_uri().'/style-editor.css', array(), '1.0', 'all');
+
+    wp_enqueue_script('jquery.min.js', get_template_directory_uri().'/assets/libs/jquery/js/jquery.min.js', array(), '3.5.1', true);
+    wp_enqueue_script('bootstrap.bundle.min.js', get_template_directory_uri().'/assets/libs/bootstrap/js/bootstrap.bundle.min.js', array(), '4.5.0', true);
+    wp_enqueue_script('fancybox.min.js', get_template_directory_uri().'/assets/libs/fancybox/js/fancybox.min.js', array(), '3.5.7', true);
+    wp_enqueue_script('owl.carousel.min.js', get_template_directory_uri().'/assets/libs/owl.carousel/js/owl.carousel.min.js', array(), '2.3.4', true);
+    wp_enqueue_script('main.min.js', get_template_directory_uri().'/assets/js/main.min.js', array(), '1.0', true);
+  }
+  add_action('wp_enqueue_scripts', 'benfiratkaya_script_enqueue');
+
+  function benfiratkaya_custom_post_type (){
+  	$labels = array(
+  		'name'                => 'Projects',
+  		'singular_name'       => 'Projects',
+  		'add_new'             => 'add new',
+  		'all_items'           => 'All projects',
+  		'add_new_item'        => 'add new',
+  		'edit_item'           => 'Edit',
+  		'new_item'            => 'add new',
+  		'view_item'           => 'view',
+  		'search_item'         => 'Search in Projects',
+  		'not_found'           => 'Project not found.',
+  		'not_found_in_trash'  => 'Project not found.',
+  		'parent_item_colon'   => 'Top'
+  	);
+  	$args = array(
+  		'labels'              => $labels,
+  		'public'              => true,
+  		'has_archive'         => true,
+  		'publicly_queryable'  => true,
+  		'query_var'           => true,
+  		'rewrite'             => true,
+  		'capability_type'     => 'post',
+      'menu_icon'           => 'dashicons-editor-code',
+  		'hierarchical'        => false,
+      'taxonomies'          => array('post_tag'),
+  		'menu_position'       => 5,
+  		'exclude_from_search' => false,
+      'supports'            => array(
+  			'title',
+  			'editor',
+  			'excerpt',
+  			'thumbnail',
+  			'revisions',
+  		)
+  	);
+  	register_post_type('proje', $args);
+  }
+  add_action('init', 'benfiratkaya_custom_post_type');
+
+  function benfiratkaya_custom_taxonomies() {
+  	$labels = array(
+  		'name'              => 'Project Categories',
+  		'singular_name'     => 'Category',
+  		'search_items'      => 'Search in Categories',
+  		'all_items'         => 'All categories',
+  		'parent_item'       => 'Top',
+  		'parent_item_colon' => 'Top:',
+  		'edit_item'         => 'Edit',
+  		'update_item'       => 'Update',
+  		'add_new_item'      => 'add new',
+  		'new_item_name'     => 'add new',
+  		'menu_name'         => 'categories'
+  	);
+  	$args = array(
+      'hierarchical'        => true,
+  		'labels'              => $labels,
+  		'show_ui'             => true,
+  		'show_admin_column'   => true,
+  		'query_var'           => true,
+  		'rewrite'             => array('slug' => 'project-category')
+  	);
+  	register_taxonomy('proje-kategori', array('proje'), $args);
+  }
+  add_action('init', 'benfiratkaya_custom_taxonomies');
+
+  function benfiratkaya_theme_setup() {
+    add_theme_support('responsive-embeds');
+
+    add_theme_support('automatic-feed-links');
+
+    add_theme_support('title-tag');
+
+    add_theme_support('custom-logo');
+
+    add_theme_support('menus');
+    register_nav_menu('primary', 'Primary Header Navigation');
+
+    add_theme_support('post-thumbnails');
+    add_image_size('my_thumbnail', 640, 360, true);
+  }
+  add_action('after_setup_theme', 'benfiratkaya_theme_setup');
+
+  /* RankMath Yorum Cevaplama Sorunu Çözümü */
+  add_filter('rank_math/frontend/remove_reply_to_com', '__return_false');
+
+  /* Contact Form Recaptcha'yı diğer sayfalarda kaldırma */
+  function remove_cf7_from_pages() {
+  	global $post;
+  	if (!isset($post->post_content) || !has_shortcode($post->post_content, 'contact-form-7')) {
+      wp_dequeue_script('contact-form-7');
+  		wp_dequeue_script('wpcf7cf-scripts');
+  		wp_deregister_script('google-recaptcha');
+  		wp_dequeue_script('google-recaptcha');
+  	}
+  }
+  add_action('wp_footer', 'remove_cf7_from_pages', 1);
+
+  function remove_cf7con_from_pages() {
+  	global $post;
+  	if (!isset( $post->post_content) || !has_shortcode($post->post_content, 'contact-form-7')) {
+  		wp_dequeue_style('cf7cf-style');
+  	}
+  }
+  add_action('wp_head', 'remove_cf7con_from_pages', 1);
+
+  require get_template_directory().'/classes/walkers/nav-menu.php';
+  require get_template_directory().'/classes/walkers/comments.php';
+
 ?>
