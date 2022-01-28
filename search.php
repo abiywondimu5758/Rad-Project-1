411d23d120fc260aeac7961c5c0ed2c10b2e862b
amanueltefera1769 <amanuelat07@gmail.com>
2022-01-28 10:33:07 +0300

    update

--- search.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<section class="section">
-  <div class="container">
-    <h1 class="header-top-text">Search Results</h1>
-    <p class="header-bottom-text"><strong><?php echo get_search_query(); ?></strong> Search results for</p>
-    <?php if (have_posts()) : ?>
-      <div class="row">
