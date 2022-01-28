0acece670b8fcebf8b9554bf5d07b2af46a6ff76
abiywondimu5758 <abiywondimu1@gmail.com>
2022-01-28 13:42:06 +0300

    category update

--- category.php
+++ category.php
@@ -9,7 +9,20 @@
           <div class="col-md-4 mb-4">
             <?php get_template_part('template-parts/cards/card', 'article'); ?>
           </div>
-        
+<?php endwhile; ?>
+      </div>
+      <div class="row mt-4">
+        <div class="col-12">
+          <?php get_template_part('template-parts/paginations/pagination', 'articles'); ?>
+        </div>
+      </div>
+    <?php else : ?>
+      <div class="row">
+        <div class="col-12">
+          <?php get_template_part('template-parts/alerts/articles', 'none'); ?>
+        </div>
+      </div>
+    <?php endif; ?>
   </div>
 </section>
 <?php get_footer(); ?>
